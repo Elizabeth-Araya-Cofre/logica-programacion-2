f5cdd33f6542a2e1d34daebb046698a062509621
Elizabeth-Araya-Cofre <elizabeth.araya@genstudents.org>
2024-05-09 15:16:38 -0400

    conversion lista

---
+++ index.js
@@ -0,0 +1,35 @@
+function inicio() {
+    let temperaturaCelsius;
+    
+    temperaturaCelsius = prompt("Ingrese la temperatura en grados Celsius:");
+
+   while (isNaN(temperaturaCelsius)) {
+        alert("Por favor, ingrese un valor numérico.");
+        temperaturaCelsius = prompt("Ingrese la temperatura en grados Celsius:");
+    }
+
+ 
+    temperaturaCelsius = parseFloat(temperaturaCelsius);
+
+    let temperaturaEnFahrenheit = convertirCelsiusAFahrenheit(temperaturaCelsius);
+    let temperaturaEnKelvin = convertirCelsiusAKelvin(temperaturaCelsius);
+
+    mostrarResultados(temperaturaEnFahrenheit, temperaturaEnKelvin);
+}
+
+function convertirCelsiusAFahrenheit(temperatura) {
+    return (temperatura * 1.8) + 32;
+}
+
+function convertirCelsiusAKelvin(temperatura) {
+    return temperatura + 273.15;
+}
+
+function mostrarResultados(temperaturaFahrenheit, temperaturaKelvin) {
+    let mensaje = "La temperatura en Fahrenheit es " + temperaturaFahrenheit.toFixed(2) + "°F\n";
+    mensaje += "La temperatura en Kelvin es " + temperaturaKelvin.toFixed(2) + "K";
+
+    console.log(mensaje);
+}
+
+inicio();
